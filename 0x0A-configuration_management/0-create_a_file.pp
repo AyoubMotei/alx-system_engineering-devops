e4ef0abd4e60d34203079de7d3cc6f2f8847a747
AyoubMotei <ayoub.motei@gmail.com>
2023-09-22 17:08:32 -0700

    updated

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,9 @@
-# Create the file /tmp/school with the specified properties
-file{'/tmp/school':
-    ensure  => 'present',
-    owner   => 'www-data',
-    group   => 'www-data',
-    mode    => '0744',
-    content => 'I love Puppet',
+# Create school file
+file { '/tmp/school':
+  ensure  => present,
+  owner   => 'www-data',
+  group   => 'www-data',
+  mode    => '0744',
+  content => 'I love Puppet',
 }
+
