e05810ca3a937a01c5307aa3bf7da56694424b0b
AyoubMotei <ayoub.motei@gmail.com>
2023-09-22 18:11:33 -0700

    fix

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,9 @@
-# Creates a file
-file { '/tmp/school':
-  ensure  => file,
+# Crate a file in /tmp
+file { 'school':
+  ensure  => present,
   path    => '/tmp/school',
+  content => 'I love Puppet',
   mode    => '0744',
   owner   => 'www-data',
   group   => 'www-data',
-  content => 'I love Puppet'
 }
